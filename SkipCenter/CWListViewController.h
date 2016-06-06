228f52a4d1ff14fc26d2aea5c5b4a8ebcd31a9ce
王聪 <2441413514@qq.com>
2016-06-06 17:06:21 +0800

    移动文件夹

--- SkipCenter/CWListViewController.h
+++ SkipCenter/CWListViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CWListViewController : UITableViewController
+@interface CWListViewController : UIViewController
 
 @end
--- SkipCenter/CWListViewController.m
+++ SkipCenter/CWListViewController.m
@@ -7,60 +7,31 @@
 //
 
 #import "CWListViewController.h"
-#import "SkipCenterTool.h"
 
 @interface CWListViewController ()
-
-@property (nonatomic, strong) NSArray *dataArray;
 
 @end
 
 @implementation CWListViewController
 
-- (NSArray *)dataArray
-{
-    if (_dataArray == nil) {
-        _dataArray = @[
-                       @{@"customtitle":@"第四页",@"action":@"CWFourViewController",@"color":[UIColor redColor]},
-                       @{@"customtitle":@"第一页",@"action":@"CWFirstViewController",@"color":[UIColor greenColor]},
-                       @{@"customtitle":@"第二页",@"action":@"CWSecondViewController",@"color":[UIColor grayColor]},
-                       @{@"customtitle":@"第三页",@"action":@"CWThirdViewController",@"color":[UIColor lightGrayColor]},
-                       @{@"customtitle":@"第四页",@"action":@"CWFourViewController",@"color":[UIColor purpleColor]}
-                       ];
-    }
-    return _dataArray;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
-    self.tableView.tableFooterView = [[UIView alloc] init];
+    // Do any additional setup after loading the view from its nib.
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    
-    return self.dataArray.count;
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
+/*
+#pragma mark - Navigation
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
-    }
-    cell.textLabel.text = self.dataArray[indexPath.row][@"customtitle"];
-    
-    return cell;
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
 }
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSString *vcName = self.dataArray[indexPath.row][@"action"];
-    [SkipCenterTool runtimePush:vcName dic:self.dataArray[indexPath.row] nav:self.navigationController];
-}
+*/
 
 @end
